16e87f237a2fc89e75a9b3239ac63742bc630092
Neng Sitiporn <jigneng1@hotmail.com>
2024-02-12 13:11:17 +0700

    added 169

---
+++ 0169-majority-element.js
@@ -0,0 +1,17 @@
+var majorityElement = function(nums) {
+    let count = 0;
+    let element = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            element = num;
+            count = 1;
+        } else if (element === num) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return element;
+};
